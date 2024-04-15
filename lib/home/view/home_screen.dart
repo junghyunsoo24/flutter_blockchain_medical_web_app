e1db1087208b16eff91c89db8e2cfaa7df11000f
kimjeongmin <kij9451@naver.com>
2024-04-15 14:46:19 +0900

    feat: 처방 내역 페이지 추가 및 메인페이지와 연결, build: expandable 설치

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/home/view/MedicalView/DetailScreen.dart';
 import '../../user/provider/user_me_provider.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -16,19 +18,91 @@ class HomeScreen extends ConsumerWidget {
     //   ),
     // );
     return Container(
-      margin: EdgeInsets.all(20.0), // 상하좌우 여백 설정
+      margin: EdgeInsets.all(5.0), // 상하좌우 여백 설정
       decoration: BoxDecoration(
         border: Border.all(
-          color: Colors.lightBlueAccent, // 테두리 색상 설정
+          color: Colors.white, // 테두리 색상 설정
           width: 2, // 테두리 두께 설정
         ),
         borderRadius: BorderRadius.circular(20), // 모서리 둥글기 설정
       ),
       child: Padding(
-        padding: EdgeInsets.all(20.0), // 내부 여백 설정
-        child: Text('내용'), // 내부 컨텐츠
+        padding: EdgeInsets.all(3.0), // 내부 여백 설정
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.lightBlueAccent),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: EdgeInsets.all(5.0),
+                child: Row(
+                  children: <Widget>[
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(Icons.local_hospital_rounded, color: Colors.red),
+                    ),
+                    Expanded(
+                      child: ExpandableText(
+                        '진료 내용',
+                        expandText: '더보기',
+                        maxLines: 2,
+                        linkColor: Colors.blueAccent,
+
+                      ),
+
+                    ),
+
+                    IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => DetailScreen()),
+                        );
+                      },
+                      icon: Icon(Icons.arrow_forward),
+                    ),
+
+                  ],
+                ),
+              ),
+
+            ),
+
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                margin: EdgeInsets.all(5.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                margin: EdgeInsets.all(5.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.lightBlueAccent),
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                margin: EdgeInsets.all(5.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+
     // return Scaffold(
     //   appBar: PreferredSize(
     //     preferredSize: Size.fromHeight(130.0),//appBar 사이즈 조정
