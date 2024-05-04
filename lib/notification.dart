a38407d136be133a6495fb4915ff01f54a4ba47d
hyunsoo24 <sunni303356@naver.com>
2024-05-04 14:36:24 +0900

    feat: 알림 클릭시 화면전환하여 데이터 확인하는것 까지 추가

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/material.dart';
 import 'second.dart';
@@ -10,13 +9,13 @@ class FlutterLocalNotification {
   FlutterLocalNotification._();
 
   static init() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('app_icon');
-    final InitializationSettings initializationSettings =
-    InitializationSettings(android: initializationSettingsAndroid);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+    final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize
+      (initializationSettings,
         onDidReceiveNotificationResponse: (payload) async {
-          onSelectNotification(payload as String?);
+        onSelectNotification(payload);
         });
   }
 
@@ -30,30 +29,52 @@ class FlutterLocalNotification {
       priority: Priority.high,
     );
 
-    const NotificationDetails notificationDetails =
-    NotificationDetails(android: androidNotificationDetails);
+    const NotificationDetails notificationDetails = NotificationDetails(android: androidNotificationDetails);
+    String payloadString = mapToQueryString(payload);
 
     await flutterLocalNotificationsPlugin.show(
       0,
       payload['title'] ?? 'No Title',
       payload['body'] ?? 'No Body',
       notificationDetails,
-      payload: payload.toString(),
+      payload: payloadString,
     );
   }
 
-  static Future onSelectNotification(String? payload) async {
+  static Future onSelectNotification(NotificationResponse payload) async {
     if (payload != null) {
-      final data = Map<String, dynamic>.from(payload.split(',') as Map);
-      Navigator.push(
-        FlutterLocalNotification.navigatorKey.currentState!.context,
-        MaterialPageRoute(
-          builder: (context) => SecondPage(payload: data),
-        ),
-      );
+      if (payload.payload != null) {
+        try {
+          final data = Map<String, dynamic>.fromEntries(
+            payload.payload!.split('&').map((item) {
+              final List<String> parts = item.split('=');
+              if (parts.length == 2) {
+                return MapEntry(Uri.decodeComponent(parts[0].trim()), Uri.decodeComponent(parts[1].trim())); // 변경된 부분
+              } else {
+                throw FormatException("잘못된 데이터 포맷입니다.");
+              }
+            })
+          );
+
+          if (navigatorKey.currentState != null) {
+            navigatorKey.currentState!.push(
+              MaterialPageRoute(
+                builder: (context) => SecondPage(payload: data),
+              ),
+            );
+          } else {
+            print('navigatorKey가 비어있습니다.');
+          }
+        } catch (e) {
+          print('데이터 파싱 중 에러 발생: $e');
+        }
+      }
     }
   }
 
-  static final GlobalKey<NavigatorState> navigatorKey =
-  GlobalKey<NavigatorState>();
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+}
+
+String mapToQueryString(Map<String, dynamic> map) {
+  return map.entries.map((entry) => '${Uri.encodeComponent(entry.key)}=${Uri.encodeComponent(entry.value.toString())}').join('&');
 }
