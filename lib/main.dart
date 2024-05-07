10fb1300fe725ea3993a0d5d6cbe59799d8e6d7a
hyunsoo24 <sunni303356@naver.com>
2024-05-07 14:48:41 +0900

    fix: main문 초기화 두번되는것 수정

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FlutterLocalNotification.init();
   runApp(
     const ProviderScope(
       child: MyApp(),
