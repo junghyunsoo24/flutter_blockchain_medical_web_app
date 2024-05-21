07971a04ab25ad7e2f0f4f0e774a8d924df2b7d9
kimjeongmin <kij9451@naver.com>
2024-05-21 22:56:26 +0900

    feat: 마이페이지 UI 설계 및 내가 등록한 게시물 조회 완료

---
+++ lib/board/view/my_board_list.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/model/question.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/repository/board_repository.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/view/board_category_list_screen.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/view/board_detail_screen.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/view/board_screen.dart';
+import 'package:portfolio_flutter_blockchain_medical_web_app/board/viewModel/board_entire_view_model.dart';
+
+final questionViewModelProvider = ChangeNotifierProvider((ref) => QuestionViewModel(QuestionRepository()));
+
+class MyBoardListScreen extends ConsumerStatefulWidget {
+
+  const MyBoardListScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MyBoardListScreen> createState() => _MyBoardListScreenState();
+}
+
+class _MyBoardListScreenState extends ConsumerState<MyBoardListScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _myBoardList("patientId");
+    //print('Received category: ${widget.category}');
+  }
+
+  void _myBoardList(String userId) {
+    //final userId = userId;
+    final userId = 'patientId';
+    ref.read(questionViewModelProvider).myBoardQuestion(userId: userId);
+    //ref.read(questionViewModelProvider).fetchQuestions(category: category, userId: userId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = ref.watch(questionViewModelProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("나의 게시글"),
+        backgroundColor: Colors.blue[50],
+        elevation: 0,
+      ),
+      body: _buildQuestionList(viewModel),
+    );
+  }
+  @override
+  Widget _buildQuestionList(QuestionViewModel viewModel) {
+    if (viewModel.questions.isEmpty) {
+      return Center(
+        child: Text(
+          '아직 등록된 게시글이 없습니다.',
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    } else {
+      return ListView.separated(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: viewModel.questions.length,
+        separatorBuilder: (context, index) => const SizedBox(height: 16.0),
+        itemBuilder: (context, index) {
+          final question = viewModel.questions[index];
+          return Container(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BoardDetailScreen(question: question),
+                  ),
+                );
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 32.0,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      question.title,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      question.content,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
+  }
+}
--- lib/data.dart
+++ lib/data.dart
@@ -10,7 +10,7 @@ const emulatorIp = '10.0.2.2:3000';
 
 //const realPhoneIp = '172.30.67.135:8080';
 const webIp = 'localhost:8080';
-const realPhoneIp = '172.30.66.77:8080';
+const realPhoneIp = '192.168.0.9:8080';
 String getIp() {
   if (Platform.isAndroid) {
     return realPhoneIp;
