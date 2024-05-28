1b6b9041586dc06e9359f2f199649fc4c1c36eab
hyunsoo24 <sunni303356@naver.com>
2024-05-28 18:15:38 +0900

    feat: 처방 내역 1차+2차인증 요청(데이터 디비 저장은 아직x)

--- lib/login/view/login_screen.dart
+++ lib/login/view/login_screen.dart
@@ -45,7 +45,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     if (response.statusCode == 200) {
       final responseBody = jsonDecode(response.body);
       if (responseBody['result'] == 'success') {
-        print('의료진 로그인 성공!');
         ref.read(userInfoProvider).setUserId(username);
         return true;
       } else {
@@ -68,7 +67,6 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     if (response.statusCode == 200) {
       final responseBody = jsonDecode(response.body);
       if (responseBody['result'] == 'success') {
-        print('환자 로그인 성공!');
         ref.read(userInfoProvider).setUserId(username);
         return true;
       } else {
@@ -262,10 +260,6 @@ class _SubTitle extends StatelessWidget {
   const _SubTitle({Key? key}) : super(key: key);
 
   @override
-
-
-
-
   Widget build(BuildContext context) {
     return const Text(
       '아이디와 비밀번호를 입력해서 로그인 해주세요!)',
